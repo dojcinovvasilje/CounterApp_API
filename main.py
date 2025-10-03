fb873cdd9565f3473d2f24f5501ddeef9af719f8
Vasilije Dojcinov <vasilije.dojcinov@intens.local>
2025-10-03 13:24:37 +0200

    Add docker hub deployment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:60621"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
